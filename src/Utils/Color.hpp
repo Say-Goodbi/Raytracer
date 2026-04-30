1ae304d0e0f830cc70b81c2c47e2d3aa51784ad6
arthur-girardin-calbe <arthur.girardin-calbe@epitech.eu>
2026-04-30 21:57:17 +0400

    feat(utils): add Color struct with arithmetic operators and clamp
    Introduces the Color struct (r, g, b as doubles) with +, -, *scalar,
    *Color operators and a clamp() method that constrains each channel
    to [0.0, 1.0].

---
+++ src/Utils/Color.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <algorithm>
+namespace RayTracer
+{
+    struct Color
+    {
+        double r;
+        double g;
+        double b;
+        Color(const Color& color) = default;
+        Color() : r(0), g(0), b(0) {}
+        Color(double r, double g, double b) : r(r), g(g), b(b) {}
+
+        Color Color::clamp(void);
+
+        Color operator*(double scalar) const
+        {
+            return Color(r * scalar, g * scalar, b * scalar);
+        }
+
+        Color operator*(const Color &other) const
+        {
+            return Color(r * other.r, g * other.g, b * other.b);
+        }
+
+        Color operator+(const Color &other) const
+        {
+            return Color(r + other.r, g + other.g, b + other.b);
+        }
+
+        Color operator-(const Color &other) const
+        {
+            return Color(r - other.r, g - other.g, b - other.b);
+        }
+    };
+} // namespace RayTracer
