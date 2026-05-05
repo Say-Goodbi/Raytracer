8393035c3432f48a0086342746db54df5ffcb301
Leslye Jeannin <leslye1.jeannin@epitech.eu>
2026-05-05 17:24:41 +0400

    feat(light): add ambient light implementation

---
+++ src/CorePlugin/Lights/AmbientLight/AmbientLight.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "../../../Objects/Abstracts/ILight.hpp"
+
+namespace RayTracer
+{
+    class AmbientLight : public ILight
+    {
+    private:
+        float _intensity;
+        Color _color;
+
+    public:
+        AmbientLight(float intensity, Color color);
+        ~AmbientLight() = default;
+
+        /// @brief Ambient light never casts shadows, just returns color * intensity
+        Color computeLight( const Geometry::HitRecord& hit, const std::vector<std::unique_ptr<APrimitive>>& primitives) const override;
+    };
+} // namespace RayTracer
