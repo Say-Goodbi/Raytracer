5401306ae5a6fd30168c3773794fa926e9fbac1c
arthur-girardin-calbe <arthur.girardin-calbe@epitech.eu>
2026-05-05 17:24:03 +0400

    feat: utils random generator - Pseudo random generator helper - Mainly used in the sampling at render time

---
+++ src/Utils/Utils.hpp
@@ -0,0 +1,21 @@
+/* ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ * EPITECH PROJECT - Wed, May, 2026                                                     *
+ * Title           - raytracer                                                          *
+ * Description     -                                                                    *
+ *     rand                                                                             *
+ * ------------------------------------------------------------------------------------ */
+
+#include <random>
+
+namespace RayTracer
+{
+    class Utils
+    {
+    public:
+        static double rng(void);
+    };
+    
+    
+} // namespace RayTracer
+
