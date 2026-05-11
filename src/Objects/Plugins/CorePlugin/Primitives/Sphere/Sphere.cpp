f6a4f9752f2bbc0bfdd4c5605807c756df442e3b
arthur-girardin-calbe <arthur.girardin-calbe@epitech.eu>
2026-05-11 13:24:27 +0400

    feat: sphere primitive

---
+++ src/Objects/Plugins/CorePlugin/Primitives/Sphere/Sphere.cpp
@@ -0,0 +1,31 @@
+#include "Sphere.hpp"
+
+namespace RayTracer
+{
+    Sphere::Sphere(Geometry::Point3D center, float radius, IMaterial *material)
+        : APrimitive(material), _center(center), _radius(radius)
+    {
+    }
+
+    std::optional<Geometry::HitRecord> Sphere::hit(const Geometry::Ray& ray) const
+    {
+        Geometry::Vector3D oc = ray.origin - _center;
+        float a = ray.direction.dot(ray.direction);
+        float h = oc.dot(ray.direction);
+        float c = oc.dot(oc) - _radius * _radius;
+        float disc = h * h - a * c;
+
+        if (disc >= 0) {
+            float sqrtDisc = std::sqrt(disc);
+            float t = (-h - sqrtDisc) / a;
+            if (t <= 1e-4f)
+                t = (-h + sqrtDisc) / a;
+            if (t > 1e-4f) {
+                Geometry::Point3D hitPoint = ray.at(t);
+                Geometry::Vector3D normal = (hitPoint - _center).normalize();
+                return Geometry::HitRecord(hitPoint, normal, t, this->getMaterial());
+            }
+        }
+        return std::nullopt;
+    }
+} // namespace RayTracer
