030867458ee6239d676e27fc13e602fc5bc4b289
Say-Goodbi <Say-Goodbi@users.noreply.github.com>
2026-05-17 07:25:08 +0000

    deploy: 8bd25f35f4c5f59497488d4abc5ed1945a444d90

--- search/all_4.js
+++ search/all_4.js
@@ -10,5 +10,6 @@ var searchData=
   ['displayframe_7',['displayFrame',['../classRayTracer_1_1Livescene.html#ae30cec23d0face3195c3be440aa0c875',1,'RayTracer::Livescene']]],
   ['divisionbyzeroexception_8',['divisionbyzeroexception',['../classRayTracer_1_1DivisionByZeroException.html',1,'RayTracer::DivisionByZeroException'],['../classRayTracer_1_1DivisionByZeroException.html#a4ea65d73351362a36824975c9e9c3b81',1,'RayTracer::DivisionByZeroException::DivisionByZeroException()']]],
   ['divisionbyzeroexception_2ehpp_9',['DivisionByZeroException.hpp',['../DivisionByZeroException_8hpp.html',1,'']]],
-  ['dot_10',['dot',['../classGeometry_1_1Vector3D.html#afd038692c679cc53b060b8c732b859e3',1,'Geometry::Vector3D::dot(const Vector3D &amp;v1, const Vector3D &amp;v2)'],['../classGeometry_1_1Vector3D.html#affb9009fe58aafa334f1f5f703feceb3',1,'Geometry::Vector3D::dot(const Vector3D &amp;other) const']]]
+  ['dot_10',['dot',['../classGeometry_1_1Vector3D.html#afd038692c679cc53b060b8c732b859e3',1,'Geometry::Vector3D::dot(const Vector3D &amp;v1, const Vector3D &amp;v2)'],['../classGeometry_1_1Vector3D.html#affb9009fe58aafa334f1f5f703feceb3',1,'Geometry::Vector3D::dot(const Vector3D &amp;other) const']]],
+  ['doxygen_5fdirs_2edox_11',['doxygen_dirs.dox',['../doxygen__dirs_8dox.html',1,'']]]
 ];
--- search/files_3.js
+++ search/files_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['directionallight_2ecpp_0',['DirectionalLight.cpp',['../DirectionalLight_8cpp.html',1,'']]],
   ['directionallight_2ehpp_1',['DirectionalLight.hpp',['../DirectionalLight_8hpp.html',1,'']]],
-  ['divisionbyzeroexception_2ehpp_2',['DivisionByZeroException.hpp',['../DivisionByZeroException_8hpp.html',1,'']]]
+  ['divisionbyzeroexception_2ehpp_2',['DivisionByZeroException.hpp',['../DivisionByZeroException_8hpp.html',1,'']]],
+  ['doxygen_5fdirs_2edox_3',['doxygen_dirs.dox',['../doxygen__dirs_8dox.html',1,'']]]
 ];
